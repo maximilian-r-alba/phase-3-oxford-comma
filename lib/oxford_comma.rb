58de8903388eaa7196529183d69224eef24c5611
Maximilian Rex Alba <albam476@gmail.com>
2022-12-04 21:38:46 -0500

    commit tree 48a551ce2347489b37b73cc7ecbfe56d7842003e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
-def oxford_comma(array)
+require 'pry'
+# def oxford_comma(array)
 
-end
+#    if array.length >= 3
+#     last_li = "and " + array.pop()
+#     new_arr = array
+#     new_arr << last_li
+#     new_arr.join(', ')
+#    elsif array.length == 1
+#     array.join()
+#     elsif array.length == 2 
+#         array.join(" and ")
+#    end
+
+# end
+
+def oxford_comma(array)
+    return array.join(" and ") if array.size < 3
+  
+    # insert 'and ' in front of the last string in the array
+    array[-1] = "and #{array[-1]}"
+  
+    array.join(", ")  
+  end
